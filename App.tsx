0c6b113ee4472310d685320facf04ec68ec5ea0a
Brandon Fitzwater <brandonfitzh2o@gmail.com>
2023-09-26 07:58:06 -0400

    chore: check if twilio is the issue

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  Conversation,
+} from "@twilio/conversations";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Bun Example App</Text>
+      <Text>{Conversation ? 'Found Twilio Class' : 'Twilio Class Not Found'}</Text>
       <StatusBar style="auto" />
     </View>
   );
